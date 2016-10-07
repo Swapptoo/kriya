3c649513662485b3fb638df90da6830a9ea55df4
vibol <vibolteav@gmail.com>
2016-10-07 14:26:46 +0700

    Fix link to chat room

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default :from => 'noreply@kriya.ai'
-  self.default_url_options = { host: "#{Rails.application.secrets.host}:3000" }
+  self.default_url_options = { host: "#{Rails.application.secrets.host}" }
 
   layout 'mailer'
 end
