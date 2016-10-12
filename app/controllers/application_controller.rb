0a332a6caf6e369113bfd417e4f6f4c63202fd32
vibol <vibolteav@gmail.com>
2016-10-12 14:06:50 +0700

    Fix redirect to sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  acts_as_token_authentication_handler_for User
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  acts_as_token_authentication_handler_for User, fallback: :none
 
   before_action do
     if current_user && current_user.email == "cqpanxu@gmail.com"
