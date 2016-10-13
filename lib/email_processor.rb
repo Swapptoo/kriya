8254a1d5dfca43043fdf28265edd338f38da38e0
Daniel A. Zorin <juan@lvk.cs.msu.su>
2016-10-13 22:36:47 +0300

    attachments from email
    Signed-off-by: Daniel A. Zorin <juan@lvk.cs.msu.su>

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -15,6 +15,20 @@ class EmailProcessor
 	num = @email.to.first[:token].tr('^0-9', '')
 	room = Room.find num
 	room.messages.create({:seen => true, :body => @email.body, :room => room, :user => room.user})
+	@email.attachments.each do |attachment|
+		room.messages.create({:seen => true, :body => '', :room => room, :user => room.user, :image => attachment})
+	end
 	room.save
+	
+	ActionCable.server.broadcast(
+        "rooms:#{room.id}:messages",
+        message: MessagesController.render(
+          partial: 'messages/message',
+          locals: {
+            message: room.messages.last, user: room.user
+          }
+        ),
+        room_id: room.id,
+      )
   end
 end
