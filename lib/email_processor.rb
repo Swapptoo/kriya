225213e926c9b7d91e34ccfcb882e5e140e62615
Daniel A. Zorin <juan@lvk.cs.msu.su>
2016-10-13 15:12:04 +0300

    email processing changed
    Signed-off-by: Daniel A. Zorin <juan@lvk.cs.msu.su>

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -4,28 +4,17 @@ class EmailProcessor
   end
 
   def process
-    # all of your application-specific code here - creating models,
-    # processing reports, etc
-    # @room.messages.create({:seen => true, :body => 'Please choose one your project timeline', :room => @room, :user => @room.manager})
-
-    # @user = User.find_by_email(@email.from[:email])
-    #
-    #
-    #
-    # # here's an example of model creation
-    # user.posts.create!(
-    #   subject: @email.subject,
-    #   body: @email.body
-    # )
-	num = @email.to.tr('^0-9', '')
-	room = Room.find num
-	room.messages.create({:seen => true, :body => @email.body, :room => room, :user => room.user})
-	room.save
-    
 	puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@"
     puts @email.subject
     puts @email.body
     puts @email.headers
+	puts @email.to
+	puts @email.from
     puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	
+	num = @email.to.first[:token].tr('^0-9', '')
+	room = Room.find num
+	room.messages.create({:seen => true, :body => @email.body, :room => room, :user => room.user})
+	room.save
   end
 end
