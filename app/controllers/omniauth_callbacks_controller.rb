66c3c87404fa49f1b52764ce145bd80186432dd9
ksbek <ksbek112@yandex.com>
2016-10-17 20:09:40 +0200

    Fix omniauth callback again

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,32 +1,24 @@
 class OmniauthCallbacksController < ApplicationController
   def create
-    if request.env["omniauth.params"]["user"] == "Freelancer"
-      [:facebook, :twitter, :linkedin].each do |provider|
-        define_method provider do
-          from_omniauth = Freelancer.from_omniauth(request.env["omniauth.auth"])
+    if request.env["omniauth.params"]["user"] == "freelancer"
+      from_omniauth = Freelancer.from_omniauth(request.env["omniauth.auth"])
 
-          if from_omniauth.is_a?(Freelancer) && from_omniauth.persisted?
-            sign_in_and_redirect from_omniauth, event: :authentication #this will throw if from_omniauth is not activated
-            # set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
-          else
-            session["devise.oauth_data"] = from_omniauth
-            redirect_to new_freelancer_registration_url
-          end
-        end
+      if from_omniauth.is_a?(Freelancer) && from_omniauth.persisted?
+        sign_in_and_redirect from_omniauth, event: :authentication #this will throw if from_omniauth is not activated
+        # set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
+      else
+        session["devise.oauth_data"] = from_omniauth
+        redirect_to new_freelancer_registration_url
       end
     else
-      [:facebook, :twitter, :linkedin].each do |provider|
-        define_method provider do
-          from_omniauth = User.from_omniauth(request.env["omniauth.auth"])
+      from_omniauth = User.from_omniauth(request.env["omniauth.auth"])
 
-          if from_omniauth.is_a?(User) && from_omniauth.persisted?
-            sign_in_and_redirect from_omniauth, event: :authentication #this will throw if from_omniauth is not activated
-            # set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
-          else
-            session["devise.oauth_data"] = from_omniauth
-            redirect_to new_user_registration_url
-          end
-        end
+      if from_omniauth.is_a?(User) && from_omniauth.persisted?
+        sign_in_and_redirect from_omniauth, event: :authentication #this will throw if from_omniauth is not activated
+        # set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
+      else
+        session["devise.oauth_data"] = from_omniauth
+        redirect_to new_user_registration_url
       end
     end
   end
