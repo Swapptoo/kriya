4e0a8f22e5871cdb5a022ac3fbf36062c84ff662
ksbek <ksbek112@yandex.com>
2016-10-19 08:57:26 +0200

    Fixes room page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :update, :destroy]
   before_action :set_goomp, only: [:new]
-  before_action :authenticate_user!
+  before_action :authenticate!
   respond_to :html, :json
   # GET /posts
   # GET /posts.json
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -28,7 +28,7 @@ class RoomsController < ApplicationController
 
   # GET /rooms/new
   def new
-    redirect_to :back unless current_user.client?
+    redirect_to root_path unless current_user.client?
     @room = Room.new
     @room.messages.new
   end
