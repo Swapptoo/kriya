9641a58fe8be3c425a7d9d7a47280d0a690172ee
vibol <vibolteav@gmail.com>
2016-10-29 01:12:39 +0700

    fix(create task): not resume task last step

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -85,7 +85,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    if resource.role == 'client'
+    if resource.client?
       task_from_sign_up_path
     else
       root_path
