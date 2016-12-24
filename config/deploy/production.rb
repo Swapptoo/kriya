67720e9325463b7fc130b160d5ac8fc3af228a61
vibol <vibolteav@gmail.com>
2016-12-24 18:02:37 +0700

    chore(prod): change cap config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :stage, :production
 set :branch, 'stable'
 set :repo_url, "git@github.com:thefantos/#{fetch(:application)}.git"
 
-server '35.166.9.27', user: 'deployer', roles: %w{app web db}
+server 'kriya.ai', user: 'deployer', roles: %w{app web db}
