a827256d0535c4a65f4f5b74783017341aa92a3f
vibol <vibolteav@gmail.com>
2016-12-28 18:49:39 +0700

    fix migration

--- db/migrate/20161129213304_add_source_to_messages.rb
+++ db/migrate/20161129213304_add_source_to_messages.rb
@@ -2,6 +2,6 @@ class AddSourceToMessages < ActiveRecord::Migration[5.0]
   def change
     add_column :messages, :source, :integer, default: 0
 
-    Message.update_all(source: 0)
+    Message.with_deleted.update_all(source: 0)
   end
 end
