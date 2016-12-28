8fb5ecbfaa56ff009507a6f566f8e71ed18fef6c
vibol <vibolteav@gmail.com>
2016-12-28 18:52:56 +0700

    fix migration

--- db/migrate/20161224143000_add_one_mn_from_previous_to_messages.rb
+++ db/migrate/20161224143000_add_one_mn_from_previous_to_messages.rb
@@ -2,7 +2,7 @@ class AddOneMnFromPreviousToMessages < ActiveRecord::Migration[5.0]
   def change
     add_column :messages, :one_mn_from_previous, :boolean, default: false
 
-    Message.includes(:user).find_each do |new_msg|
+    Message.with_deleted.includes(:user).find_each do |new_msg|
       message = new_msg.previous_message
       new_msg.update_columns(one_mn_from_previous: true) if !message.nil? && message.user == new_msg.user && new_msg.seconds_from_message(message) <= 60
     end
