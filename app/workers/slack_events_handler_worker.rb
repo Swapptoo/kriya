819f514a6767a1e9ef5cfd9806941b139eb72c7c
vibol <vibolteav@gmail.com>
2016-12-30 17:07:44 +0700

    fix(slack): ignore bot message from slack to kriya

--- app/workers/slack_events_handler_worker.rb
+++ app/workers/slack_events_handler_worker.rb
@@ -9,7 +9,7 @@ class SlackEventsHandlerWorkerSyncWorker
 
     slack_channel = SlackChannel.find_by(uid: authed_users, channel_id: slack_event.channel)
 
-    return if slack_channel.nil? || slack_event.hidden == true
+    return if slack_channel.nil? || slack_event.hidden == true || slack_event.subtype.present?
 
     message_owner = slack_channel.user.presence || slack_channel.freelancer
     body = Slack::Messages::Formatting.unescape(slack_event.text)
