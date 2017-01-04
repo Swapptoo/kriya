608c5dc39ff9f4ad1452b6c78f13fdcafa308395
vibol <vibolteav@gmail.com>
2017-01-04 21:20:35 +0700

    fix(): cron job

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/#{fetch(:application)}"
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
